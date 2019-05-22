d06fb10993c10e3f09986f868e4aa6e19756b25b
Alex Ruiz <ruiz.alex0302@gmail.com>
2019-05-21 23:05:40 -0700

    Mechanics functional

--- src/game.js
+++ src/game.js
@@ -2,6 +2,7 @@ import player from './assets/img/player.png';
 import bg from './assets/img/bg.png';
 import coin from './assets/img/coin.png';
 import police from './assets/img/police.png';
+import person from './assets/img/person.png'
 
 import * as PIXI from 'pixi.js';
 import * as Intersects from 'yy-intersects';
@@ -19,7 +20,8 @@ class Game {
     this.textures = {};
     this.stats = {
       cash: 50,
-      voterPercent: .2
+      voterPercent: .2,
+      suspicion: .2
     };
     this.input = {
       up: false,
@@ -34,7 +36,8 @@ class Game {
       .add('player', player)
       .add('bg', bg)
       .add('coin', coin)
-      .add('police', police);
+      .add('police', police)
+      .add('person', person);
 
     //Load things
     this.app.loader.load((loader, resources) => {
@@ -42,6 +45,7 @@ class Game {
       this.sprites.bg = new PIXI.TilingSprite(resources.bg.texture);
       this.textures.coin = resources.coin.texture;
       this.textures.police = resources.police.texture;
+      this.textures.person = resources.person.texture;
       this.setupSprites();
     });
   }
@@ -57,8 +61,8 @@ class Game {
       this.app.stage.addChild(this.sprites.player);
       this.sprites.player.height = 100;
       this.sprites.player.width = 100;
-      this.sprites.player.anchor.set(0,.5);
-      this.sprites.player.y = this.height / 2;
+      this.sprites.player.anchor.set(0);
+      this.sprites.player.y = this.height / 2 - this.sprites.player.height/2;
       this.sprites.player.x = this.width / 2 - this.sprites.player.width/2;
 
       //container for moving objects
@@ -67,27 +71,48 @@ class Game {
       this.app.stage.addChild(movingObjects);
 
       this.tick = 0;
-      var bgspeed = 4;
+      var bgspeed = 1;
       var speed = 2.5;
       this.app.ticker.add(()=>{
         //move background
         this.sprites.bg.tilePosition.x-=bgspeed
 
+        //check for lose
+        if(this.stats.suspicion >= 1)
+          this.app.stop();
+        if(this.stats.cash <= 0)
+          this.app.stop();
+
         //random chance events
-        if (this.tick % 30 === 0){
+        if (this.tick % 25 === 0){
           this.tick = 0;
+
+          if(this.stats.suspicion > this.stats.voterPercent)
+            this.stats.suspicion-=.005;
+          else this.stats.suspicion = this.stats.voterPercent;
+          this.stats.cash-=this.stats.voterPercent*3;
+
+          this.stats.voterPercent-=.002;
+          //police event
+          if (Math.random() < this.stats.suspicion*.6){
+            let sprite = new PIXI.Sprite(this.textures.police);
+            sprite.name = "police";
+            sprite.x = 640;
+            sprite.y = 10 + Math.floor(Math.random()*(460-sprite.height));
+            movingObjects.addChild(sprite);
+          }
           //coin event
-          if (Math.random() < this.stats.voterPercent*1.2){
+          else if (Math.random() < this.stats.voterPercent*1.5){
             let sprite = new PIXI.Sprite(this.textures.coin);
             sprite.name = "coin";
             sprite.x = 640;
             sprite.y = 10 + Math.floor(Math.random()*(460-sprite.height));
             movingObjects.addChild(sprite);
           }
-          //police event
-          if (Math.random() < this.stats.voterPercent*1){
-            let sprite = new PIXI.Sprite(this.textures.police);
-            sprite.name = "police";
+          //person event
+          else if (Math.random() < .4){
+            let sprite = new PIXI.Sprite(this.textures.person);
+            sprite.name = "person";
             sprite.x = 640;
             sprite.y = 10 + Math.floor(Math.random()*(460-sprite.height));
             movingObjects.addChild(sprite);
@@ -97,7 +122,7 @@ class Game {
 
         //move children and check for out of bounds
         for (let sprite of movingObjects.children){
-          sprite.x-=bgspeed;
+          sprite.x-=(bgspeed+2);
           if (sprite.x < -1*sprite.width)
             movingObjects.removeChild(sprite);
         }
@@ -108,10 +133,10 @@ class Game {
 
         //player input -> movement
         if (this.input.up
-          && this.sprites.player.y > this.sprites.player.height/2)
+          && this.sprites.player.y > 0)
           this.sprites.player.y-=speed;
         if (this.input.down
-          && this.sprites.player.y < 480 - this.sprites.player.height/2)
+          && this.sprites.player.y < 480 - this.sprites.player.height)
           this.sprites.player.y+=speed;
         if(this.input.left
           && this.sprites.player.x > 0)
@@ -121,7 +146,8 @@ class Game {
           this.sprites.player.x+=(bgspeed + speed);
 
         //intersection
-        let playershape = new Intersects.Rectangle(this.sprites.player);
+        let player = this.sprites.player;
+        let playershape = new Intersects.Rectangle(player);
         for (let sprite of movingObjects.children){
           let spriteshape = new Intersects.Rectangle(sprite);
           if (playershape.collidesRectangle(spriteshape)){
@@ -129,6 +155,17 @@ class Game {
               this.stats.cash+=10;
               console.log("money: "+this.stats.cash);
               movingObjects.removeChild(sprite);
+            }
+            if (sprite.name === "person"){
+              this.stats.voterPercent+=.02;
+              console.log("voterPercent: "+this.stats.voterPercent);
+              movingObjects.removeChild(sprite);
+            }
+            if (sprite.name === "police"){
+              this.stats.suspicion+=.2;
+              this.stats.voterPercent-=.05;
+              console.log("suspicion: "+this.stats.suspicion);
+              sprite.name = "suspiciouspolice";
             }
           }
         }
